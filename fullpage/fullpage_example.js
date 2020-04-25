4dad75c1745404b4d5d795503592dad2659427a6
buharov-alexander <buharovsasha@yandex.ru>
2020-04-25 21:50:41 +0700

    add fullpage example

---
+++ fullpage/fullpage_example.js
@@ -0,0 +1,7 @@
+const myFullpage = new fullpage('#fullpage', {
+  anchors: ['firstPage', 'secondPage', '3rdPage'],
+  sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+  navigation: true,
+  navigationPosition: 'right',
+  navigationTooltips: ['First page', 'Second page', 'Third and last page']
+});
